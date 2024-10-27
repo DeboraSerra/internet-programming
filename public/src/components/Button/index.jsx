1c65c50eac2229c016a0671d87de7135305a5c74
Bruno <brunosca96@gmail.com>
2024-10-26 18:38:03 -0700

    add password validation

--- public/src/components/Button/index.jsx
+++ public/src/components/Button/index.jsx
@@ -11,15 +11,71 @@ function Button({
 }) {
   const router = useRouter();
 
-  function validadeEmail(email) {
+  function validateEmail(email) {
     //add email validation
     return true;
   }
   function validatePassword(password) {
-    //add password validation
-    return true;
-  }
+    /**
+     * first rule: 8 characters long at least.
+     * Second rule: At least 1 special characters.
+     * Third rule: One Caps letter.
+     * Forth rule: One lower case letter. 
+     * Fifth rule: Require a number.
+     */
+    let isValid = false
+    if(password.length < 8) {
+      isValid = false
+    }
+    else {
+      isValid = true
+    }
+    const specialCharacters = ["@", "!", "?", "¡", "¿", "$", "%", "&", "(", ")"]
+    for(const character of specialCharacters) {
 
+      if(!password.includes(character)){
+        isValid = false
+      }
+      else {
+        isValid = true
+        break
+      }
+    }
+    const letters = ["A","B","C","D","E","F","G","H","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Z"]
+    for(const character of letters) {
+
+      if(!password.includes(character)){
+        isValid = false
+      }
+      else {
+        isValid = true
+        break
+      }
+    } 
+    for(const character of letters) {
+
+      if(!password.includes(character.toLowerCase())){
+        isValid = false
+      }
+      else {
+        isValid = true
+        break
+      }
+    } 
+    const numbers = ["0","1","2","3","4","5","6","7","8","9"]
+    for(const number of numbers) {
+
+      if(!password.includes(number)){
+        isValid = false
+      }
+      else {
+        isValid = true
+        break
+      }
+    } 
+    return isValid;
+  }
+  
   const redirect = (url) => {
     if (url) {
       router.push(url);
@@ -30,7 +86,7 @@ function Button({
     const form = document.querySelector("form");
     const email = form.querySelector("input#email").value;
     const password = form.querySelector("input#password").value;
-    if (!validadeEmail(email)) return;
+    if (!validateEmail(email)) return;
     if (!validatePassword(password)) return;
     const url = await auth.passwordLogin({ email, password });
     redirect(url);
@@ -45,7 +101,7 @@ function Button({
     const form = document.querySelector("form");
     const email = form.querySelector("input#email").value;
     const password = form.querySelector("input#password").value;
-    if (!validadeEmail(email)) return;
+    if (!validateEmail(email)) return;
     if (!validatePassword(password)) return;
     const url = await auth.createAccount({ email, password });
     redirect(url);
