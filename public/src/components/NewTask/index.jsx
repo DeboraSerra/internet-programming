c5046fa615389f2184f90742e4edce54e7baa1a6
Debora Serra <debora.r.serra@gmail.com>
2024-11-12 14:16:38 -0800

    fix: minor changes to form

--- public/src/components/NewTask/index.jsx
+++ public/src/components/NewTask/index.jsx
@@ -1,44 +1,77 @@
-import service from "@/backend/services/tasks.mjs";
-<link rel="stylesheet" href="style.css" />
+"use client";
+import { useState } from "react";
+import "./style.css";
 
 function NewTask() {
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [priority, setPriority] = useState(0);
+  const [priority, setPriority] = useState(1);
   const [repeat, setRepeat] = useState(false);
+
   return (
     <div>
-      <h1>new task</h1>
-      <form action="">
-     
-        <label id="date">Date</label>
-        <input type="date" value={Date} id="date"/>
+      <form action=''>
+      <h1>New task</h1>
+        <label htmlFor='date'>Date</label>
+        <input
+          type='date'
+          value={date}
+          id='date'
+          onChange={(e) => setDate(e.target.value)}
+          min={new Date().toISOString().split("T")[0]}
+        />
 
-        <label id="time">Hour</label>
-        <input type="time" value={time} id="time"/>
-          
-        <label id="name" >Name</label>
-        <input type="text" value={name} id="name"/>
+        <label htmlFor='time'>Hour</label>
+        <input
+          type='time'
+          value={time}
+          id='time'
+          onChange={(e) => setTime(e.target.value)}
+        />
 
-        <label id="description" >Description</label>
-        <textarea value={description} id="Description" ></textarea>
+        <label htmlFor='name'>Name</label>
+        <input
+          type='text'
+          value={name}
+          id='name'
+          onChange={(e) => setName(e.target.value)}
+        />
 
-        <label id="priority" >Priority</label>
-        <input type="range" min="0" max="10" value={priority} id="priority" />
+        <label htmlFor='description'>Description</label>
+        <textarea
+          value={description}
+          id='Description'
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
 
-      <div>
-          <input type="checkbox" />
-          <label >Repeat event</label>
+        <label htmlFor='priority'>Priority {priority}</label>
+        <input
+          type='range'
+          min='1'
+          max='5'
+          value={priority}
+          id='priority'
+          onChange={(e) => setPriority(e.target.value)}
+        />
+
+        <div>
+          <label htmlFor='repeat'>
+            <input
+              type='checkbox'
+              id='repeat'
+              value={repeat}
+              onChange={(e) => setRepeat(e.target.checked)}
+            />{' '}
+            Repeat event
+          </label><br/>
+          <label htmlFor="times" id="times-label">Number of times<input type="number" name="times" id="times" disabled={!repeat} /></label>
         </div>
 
-        <button type="submit"> Submit </button>
-
-    </form>
+        <button type='submit' className="submit"> Submit </button>
+      </form>
     </div>
-   
-
   );
 }
 
