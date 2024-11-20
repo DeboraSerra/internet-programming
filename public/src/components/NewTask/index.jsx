5d7f78c629300bbf224a7b9697f64ad0901a993a
Debora Serra <debora.r.serra@gmail.com>
2024-11-20 12:51:40 -0800

    feat: update new task and edit task features

--- public/src/components/NewTask/index.jsx
+++ public/src/components/NewTask/index.jsx
@@ -1,58 +1,118 @@
 "use client";
 import constants from "@/script/constants";
-import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
+import Input from "../ControlledInput";
+import toastEmitter, { TOAST_EMITTER_KEY } from "../Toast/toastEmitter";
 import "./style.css";
 
-const newTaskUrl = constants.TASK_URL + "?userId=67311a177bb55a6ce6df4947";
+const newTaskUrl = constants.TASK_URL + "?userId=";
+
+const defaultState = {
+  date: "",
+  name: "",
+  description: "",
+  priority: 1,
+  repeat: false,
+  times: 1,
+};
 
 function NewTask() {
-  const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
-  const [name, setName] = useState("");
-  const [description, setDescription] = useState("");
-  const [priority, setPriority] = useState(1);
-  const [repeat, setRepeat] = useState(false);
+  const [state, setState] = useState(defaultState);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const { date, description, name, times, priority, repeat } = state;
+  const router = useRouter();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const editTask = localStorage.getItem("editTask");
+    if (editTask) {
+      setState((prev) => ({ ...prev, ...JSON.parse(editTask) }));
+      setIsEdit(true);
+    } else {
+      setState(defaultState);
+      setIsEdit(false);
+    }
+  }, []);
+
+  const handleChange = ({ target }) => {
+    const { name, checked } = target;
+    let { value } = target;
+    if (name === "repeat") value = checked;
+    setState((prev) => ({ ...prev, [name]: value }));
+  };
 
   async function submitForm(e) {
+    setIsLoading(true);
     e.preventDefault();
-    console.log("prevent");
+    const numOfTimes = parseInt(times);
+    let taskDate = new Date(date);
+    for (let i = 1; i <= numOfTimes; i += 1) {
+      try {
+        const url = isEdit
+          ? `${constants.TASK_URL}/${state.id}?userId=${id}`
+          : newTaskUrl + id;
+        const result = await fetch(url, {
+          method: isEdit ? "PUT" : "POST",
+          body: JSON.stringify({
+            name,
+            description,
+            date: taskDate,
+            priority,
+            userId: id,
+          }),
+        });
+        await result.json();
+        toastEmitter.emit(
+          TOAST_EMITTER_KEY,
+          `Task to day ${taskDate.toLocaleDateString()} ${
+            isEdit ? "updated" : "created"
+          }`
+        );
+      } catch (err) {
+        console.log(err);
+        toastEmitter.emit(
+          TOAST_EMITTER_KEY,
+          `Task to day ${taskDate.toLocaleDateString()} could not be ${
+            isEdit ? "updated" : "created"
+          }`
+        );
+      }
+      taskDate.setDate(taskDate.getDate() + 1);
+    }
+    setIsLoading(false);
+    router.push(`/${id}/dashboard`);
   }
 
   return (
     <div>
       <form action='' onSubmit={submitForm}>
         <h1>New task</h1>
-        <label htmlFor='date'>Date</label>
-        <input
+        <Input
           type='date'
-          value={date}
+          value={date.split("T")[0]}
           id='date'
-          onChange={(e) => setDate(e.target.value)}
+          label='Date'
+          onChange={handleChange}
           min={new Date().toISOString().split("T")[0]}
         />
-
-        <label htmlFor='time'>Hour</label>
-        <input
-          type='time'
-          value={time}
-          id='time'
-          onChange={(e) => setTime(e.target.value)}
-        />
-
-        <label htmlFor='name'>Name</label>
-        <input
+        <Input
           type='text'
+          label='Name'
           value={name}
           id='name'
-          onChange={(e) => setName(e.target.value)}
+          onChange={handleChange}
         />
 
-        <label htmlFor='description'>Description</label>
-        <textarea
+        <Input
+          type='textarea'
+          label='Description'
           value={description}
-          id='Description'
-          onChange={(e) => setDescription(e.target.value)}
-        ></textarea>
+          id='description'
+          onChange={handleChange}
+        />
 
         <label htmlFor='priority'>Priority {priority}</label>
         <input
@@ -61,29 +121,38 @@ function NewTask() {
           max='5'
           value={priority}
           id='priority'
-          onChange={(e) => setPriority(e.target.value)}
+          name='priority'
+          onChange={handleChange}
         />
 
-        <div>
-          <label htmlFor='repeat'>
-            <input
-              type='checkbox'
-              id='repeat'
-              value={repeat}
-              onChange={(e) => setRepeat(e.target.checked)}
-            />{" "}
-            Repeat event
-          </label>
-          <br />
-          <label htmlFor='times' id='times-label'>
-            Number of times
-            <input type='number' name='times' id='times' disabled={!repeat} />
-          </label>
-        </div>
+        <label htmlFor='repeat'>
+          <input
+            type='checkbox'
+            id='repeat'
+            value={repeat}
+            name='repeat'
+            onChange={handleChange}
+            disabled={isEdit}
+          />{" "}
+          Repeat event
+        </label>
 
-        <button type='submit' className='submit'>
-          {" "}
-          Submit{" "}
+        <Input
+          type='number'
+          name='times'
+          label='Number of times'
+          id='times'
+          disabled={!repeat}
+          value={times}
+          onChange={handleChange}
+          min={1}
+        />
+
+        <button
+          type='submit'
+          className='flex items-center justify-center shadow-lg bg-slate-400 w-full py-3 rounded hover:bg-slate-500 active:bg-slate-600 active:shadow-none'
+        >
+          {isLoading ? <FaSpinner className='animate-spin' /> : "Submit"}
         </button>
       </form>
     </div>
