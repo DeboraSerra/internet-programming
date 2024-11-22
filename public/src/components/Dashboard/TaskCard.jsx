bff27f5f9c9115b1dce807a9962da0e6b511c7b5
Debora Serra <debora.r.serra@gmail.com>
2024-11-21 18:48:27 -0800

    fix: change user reply on dashboard

--- public/src/components/Dashboard/TaskCard.jsx
+++ public/src/components/Dashboard/TaskCard.jsx
@@ -1,42 +1,122 @@
+import PropType from "prop-types";
 import { useState } from "react";
 import { FaSpinner, FaStar, FaTrash } from "react-icons/fa";
 
-const TaskCard = ({ task, onDelete, onChecked }) => {
-    const [isHovering, setIsHovering] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
-    const getPriorityClass = (priority) => {
-        if (priority === 1) return 'circumstantial'
-        if (priority === 5) return 'urgent'
-        return 'important'
-    }
-    return isLoading ? <div className="w-full h-[85px] flex items-center justify-center"><FaSpinner className="animate-spin" /></div> : <div onMouseEnter={() => setIsHovering(true)} onMouseLeave={() => setIsHovering(false)} className="task">
-        <div className="task-header flex items-center gap-3 py-2 px-3 border-b border-slate-400">
-            <input
-                type="checkbox"
-                checked={task.completed}
-                readOnly
-                className="task-checkbox" onChange={async () => {
-                    setIsLoading(true)
-                    await onChecked(task.id, task.userId)
-                    setIsLoading(false)
-                }}
-            />
-            <span className="task-name grow">{task.name}</span>
-            <span className="cursor-pointer w-[18px]">{isHovering && <FaTrash onClick={async () => {
-                setIsLoading(true)
-                await onDelete(task.id, task.userId)
-                setIsLoading(false)
-            }
+const TaskCard = ({ task, tasks, setTasks }) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const getPriorityClass = (priority) => {
+    if (priority === 1) return "circumstantial";
+    if (priority === 5) return "urgent";
+    return "important";
+  };
 
-            } />}</span>
-            <span className={`task-priority ${getPriorityClass(task.priority)}`}>
-                <FaStar />
-            </span>
-        </div>
-        <div className="task-details border-b border-slate-400">
-            <span className="task-desc">{task.description}</span>
-        </div>
+  async function onDelete(taskId, userId) {
+    setIsLoading(true);
+    const url = constants.TASK_URL + `/${taskId}?userId=${userId}`;
+    const result = await fetch(url, { method: "DELETE" });
+    const data = await result.json();
+    if (data.task) {
+      const newList = [];
+      for (const item of tasks) {
+        if (item.id !== data.task.id) {
+          newList.push(item);
+        }
+      }
+      setTasks(newList);
+    } else {
+      toastEmitter(TOAST_EMITTER_KEY, "Task couldn't be deleted, try again");
+    }
+    setIsLoading(false);
+  }
+
+  async function onChecked(taskId, userId) {
+    const url = constants.TASK_URL + `/${taskId}?userId=${userId}`;
+    const result = await fetch(url, { method: "PATCH" });
+    const data = await result.json();
+    if (data.task) {
+      const newList = [];
+      for (const item of tasks) {
+        if (item.id !== data.task.id) {
+          newList.push(item);
+        } else {
+          newList.push({ ...item, completed: !item.completed });
+        }
+      }
+      setTasks(newList);
+    } else {
+      toastEmitter(TOAST_EMITTER_KEY, "Task couldn't be updated, try again");
+    }
+    setIsLoading(false);
+  }
+
+  return isLoading ? (
+    <div className='w-full h-[85px] flex items-center justify-center'>
+      <FaSpinner className='animate-spin' />
     </div>
-}
+  ) : (
+    <div
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      className='task'
+    >
+      <div className='task-header flex items-center gap-3 py-2 px-3 border-b border-slate-400'>
+        <input
+          type='checkbox'
+          checked={task.completed}
+          readOnly
+          className='task-checkbox'
+          onChange={async () => {
+            setIsLoading(true);
+            await onChecked(task.id, task.userId);
+            setIsLoading(false);
+          }}
+        />
+        <span className='task-name grow'>{task.name}</span>
+        <span className='cursor-pointer w-[18px]'>
+          {isHovering && (
+            <FaTrash
+              onClick={async () => {
+                setIsLoading(true);
+                await onDelete(task.id, task.userId);
+                setIsLoading(false);
+              }}
+            />
+          )}
+        </span>
+        <span className={`task-priority ${getPriorityClass(task.priority)}`}>
+          <FaStar />
+        </span>
+      </div>
+      <div className='task-details border-b border-slate-400'>
+        <span className='task-desc'>{task.description}</span>
+      </div>
+    </div>
+  );
+};
+
+TaskCard.propTypes = {
+  task: PropType.shape({
+    id: PropType.number,
+    name: PropType.string,
+    description: PropType.string,
+    priority: PropType.number,
+    completed: PropType.bool,
+    date: PropType.string,
+    userId: PropType.number,
+  }).isRequired,
+  tasks: PropType.arrayOf(
+    PropType.shape({
+      id: PropType.number,
+      name: PropType.string,
+      description: PropType.string,
+      priority: PropType.number,
+      completed: PropType.bool,
+      date: PropType.string,
+      userId: PropType.number,
+    })
+  ).isRequired,
+  setTasks: PropType.func.isRequired,
+};
 
 export default TaskCard;
--- public/src/components/Dashboard/index.jsx
+++ public/src/components/Dashboard/index.jsx
@@ -1,29 +1,25 @@
 "use client";
 
-
-import React, { useEffect, useState } from "react";
 import constants from "@/script/constants";
-import "./style.css";
-import { FaSpinner, FaStar, FaTrash } from "react-icons/fa";
+import Link from "next/link";
 import { useParams, useRouter } from "next/navigation";
-import toastEmitter, { TOAST_EMITTER_KEY } from "../Toast/toastEmitter";
+import { useEffect, useState } from "react";
+import { FaSpinner } from "react-icons/fa";
+import "./style.css";
 import TaskCard from "./TaskCard";
 
-const dashboardUrl = constants.TASK_URL
+const dashboardUrl = constants.TASK_URL;
 
 async function fetchDashboardData(id) {
   try {
-    const url = dashboardUrl + "?userId=" + id
+    const url = dashboardUrl + "?userId=" + id;
     const response = await fetch(url);
 
-    
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
+
     const data = await response.json();
-    console.log("Dashboard data fetched successfully:", data);
-    console.log({data})
     return data;
   } catch (error) {
     console.error("Failed to fetch dashboard data:", error);
@@ -33,113 +29,94 @@ async function fetchDashboardData(id) {
 
 function Dashboard() {
   const [tasks, setTasks] = useState([]);
-  const [isLoading, setIsLoading] = useState(true)
+  const [isLoading, setIsLoading] = useState(true);
   const router = useRouter();
   const { id } = useParams();
 
   async function loadData() {
     const data = await fetchDashboardData(id);
     if (!data.tasks.length) {
-      router.push(`/${id}/new-task`)
+      router.push(`/${id}/new-task`);
     } else if (data.tasks) {
       setTasks(parseTasks(data));
     }
-    setIsLoading(false)
   }
   useEffect(() => {
     loadData();
   }, []);
 
-  async function handleDelete(taskId, userId){
-    setIsLoading(true)
-    const url = constants.TASK_URL + `/${taskId}?userId=${userId}`
-    const result = await fetch(url, {method:'DELETE'})
-    const data = await result.json()
-    if(data.task){
-      const newList = []
-      for (const item of tasks) {
-        if (item.id !== data.task.id) {
-          newList.push(item)
-        }
-      }
-      setTasks(newList)
-    }else{
-      toastEmitter(
-        TOAST_EMITTER_KEY, "Task couldn't be deleted, try again"
-      )
-    }
-    setIsLoading(false)
-  }
-async function handlePatch(taskId, userId){
-  const url = constants.TASK_URL + `/${taskId}?userId=${userId}`
-  const result = await fetch(url, {method:'PATCH'})
-  const data = await result.json()
-    if(data.task){
-      const newList = []
-      for (const item of tasks) {
-        if (item.id !== data.task.id) {
-          newList.push(item)
-        } else {
-          newList.push({...item, completed: !item.completed})
-        }
-      }
-      setTasks(newList)
-    }else{
-      toastEmitter(
-        TOAST_EMITTER_KEY, "Task couldn't be updated, try again"
-      )
-    }
-    setIsLoading(false)
-  
-}
-
-
   function parseTasks(data) {
     const filteredTasks = {};
-  for (let i = 0; i < data.tasks.length; i += 1) {
-    const currTask = data.tasks[i];
-    const currDate = currTask.date;
-    if (filteredTasks[currDate]) {
-      filteredTasks[currDate].push(currTask);
-    } else {
-      filteredTasks[currDate] = [currTask];
+    for (const currTask of data.tasks) {
+      const currDate = currTask.date;
+      if (filteredTasks[currDate]) {
+        filteredTasks[currDate].push(currTask);
+      } else {
+        filteredTasks[currDate] = [currTask];
+      }
     }
+
+    const arrayOfTasks = Object.entries(filteredTasks);
+    const parsedTasks = arrayOfTasks.map(([date, taskList]) => ({
+      date,
+      tasks: taskList,
+    }));
+    setIsLoading(false);
+    return parsedTasks.filter((it) => {
+      const taskDate = new Date(it.date).toDateString();
+      const today = new Date().toDateString();
+      return new Date(taskDate) >= new Date(today);
+    });
   }
 
-  const arrayOfTasks = Object.entries(filteredTasks);
-  const parsedTasks = arrayOfTasks.map(([date, taskList]) => ({
-    date,
-    tasks: taskList,
-  }));
-  return parsedTasks.filter((it) =>{ 
-  const taskDate = new Date(it.date).toDateString()
-  const today = new Date().toDateString()
-  console.log({task: new Date(taskDate), today: new Date(today)})
-  return new Date(taskDate) >= new Date(today)})
-  }
+  console.log({ tasks });
 
   if (!isLoading && !tasks.length) {
-    router.push(`/${id}/new-task`)
+    return (
+      <div className='min-h-96 flex flex-col items-center justify-center gap-5'>
+        <p className='max-w-[500px] text-center flex items-center text-slate-800 text-2xl'>
+          You don&apos;t have any task yet or all your tasks were due before{" "}
+          {new Date().toDateString()}
+        </p>
+        <Link
+          className='flex items-center text-blue-500 text-xl underline hover:text-blue-600'
+          href={`/${id}/new-task`}
+        >
+          Add a new task
+        </Link>
+      </div>
+    );
   }
 
   if (isLoading) {
-    return <div><FaSpinner className="animate-spin" /></div>
+    return (
+      <div className='min-h-96 flex items-center justify-center'>
+        <FaSpinner className='animate-spin' />
+      </div>
+    );
   }
 
   return (
-    <div className="dashboard py-3 px-8">
-      <div className="dashboard-header"></div>
+    <div className='dashboard py-3 px-8'>
+      <div className='dashboard-header'></div>
       <div>
-        {tasks.sort((a,b) => new Date(a.date) - new Date(b.date)).map((it, index) => (
-          <div key={index}>
-            <h2 className="date-header font-bold text-lg py-3 border-b border-slate-400">
-              {new Date(it.date).toLocaleDateString()}
-            </h2>
-            {it.tasks.map((task) => (
-              <TaskCard task={task} onDelete={handleDelete} onChecked={handlePatch} key={task.id} />
-            ))}
-          </div>
-        ))}
+        {tasks
+          .sort((a, b) => new Date(a.date) - new Date(b.date))
+          .map((it, index) => (
+            <div key={index}>
+              <h2 className='date-header font-bold text-lg py-3 border-b border-slate-400'>
+                {new Date(it.date).toLocaleDateString()}
+              </h2>
+              {it.tasks.map((task) => (
+                <TaskCard
+                  task={task}
+                  key={task.id}
+                  tasks={tasks}
+                  setTasks={setTasks}
+                />
+              ))}
+            </div>
+          ))}
       </div>
     </div>
   );
