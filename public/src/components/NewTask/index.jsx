4f34e8c748f7195d87d6664c96c99e64540776f8
Debora Serra <debora.r.serra@gmail.com>
2024-11-21 17:27:06 -0800

    feat: add step by step for new task creation

--- public/src/components/NewTask/index.jsx
+++ public/src/components/NewTask/index.jsx
@@ -7,13 +7,21 @@ const newTaskUrl = constants.TASK_URL + "?userId=67311a177bb55a6ce6df4947";
 
 function NewTask() {
   const [date, setDate] = useState("");
-  const [time, setTime] = useState("");
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [priority, setPriority] = useState(1);
   const [repeat, setRepeat] = useState(false);
+  const [times, setTimes] = useState(1);
 
   async function submitForm(e) {
+    // validate fields
+    // check if repeat is true
+    // run the the fetch method "POST" the selected amount of times of repeat
+    // if repeat is true: increment date by one for each task `times` amount
+    // const taskDate = new Date(date);
+    // taskDate.setDate(taskDate.getDate() + 1)
+    // else: transform date and make the fetch one time
+    // after the end of the last fetch, redirect page to dashboard
     e.preventDefault();
     console.log("prevent");
   }
@@ -29,14 +37,6 @@ function NewTask() {
           id='date'
           onChange={(e) => setDate(e.target.value)}
           min={new Date().toISOString().split("T")[0]}
-        />
-
-        <label htmlFor='time'>Hour</label>
-        <input
-          type='time'
-          value={time}
-          id='time'
-          onChange={(e) => setTime(e.target.value)}
         />
 
         <label htmlFor='name'>Name</label>
@@ -77,7 +77,14 @@ function NewTask() {
           <br />
           <label htmlFor='times' id='times-label'>
             Number of times
-            <input type='number' name='times' id='times' disabled={!repeat} />
+            <input
+              type='number'
+              name='times'
+              id='times'
+              disabled={!repeat}
+              value={times}
+              onChange={(e) => setTimes(e.target.value)}
+            />
           </label>
         </div>
 
