ece20ca9de03cf861ad6dcb6b316f2ce0d02a85f
Debora Serra <debora.r.serra@gmail.com>
2024-11-28 14:35:12 -0800

    fix: remove headers

--- public/next.config.mjs
+++ public/next.config.mjs
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
-          },
-        ],
-      },
-    ];
-  },
+
 };
 
 export default nextConfig;
